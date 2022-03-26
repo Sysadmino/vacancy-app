f6155bd811b6071774bcb7572aa59739d8be03aa
Your Name <you@example.com>
2022-03-26 22:18:12 +0300

    Updates

---
+++ precache-manifest.abc66857cd3f4c20840c1b7c3da28688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b2e029f1b3eb4d912a5074c72d2c9c",
+    "url": "/vacancy-app/index.html"
+  },
+  {
+    "revision": "a3870e172b1b09174317",
+    "url": "/vacancy-app/static/css/main.aabf14a8.chunk.css"
+  },
+  {
+    "revision": "052030cc69829b0e59bf",
+    "url": "/vacancy-app/static/js/2.05a29364.chunk.js"
+  },
+  {
+    "revision": "abc28c365c609ffc9a2e2e165c7e73d8",
+    "url": "/vacancy-app/static/js/2.05a29364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3870e172b1b09174317",
+    "url": "/vacancy-app/static/js/main.e5c221e9.chunk.js"
+  },
+  {
+    "revision": "7cbf9e83dc27e1b25259",
+    "url": "/vacancy-app/static/js/runtime-main.c4c150de.js"
+  }
+]);
